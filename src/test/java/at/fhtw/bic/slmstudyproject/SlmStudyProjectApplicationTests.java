50cdf9f5d07e50d04b24424d1f6e25ab82cc0999
HackXIt <hackxit@gmail.com>
2022-09-18 15:32:44 +0200

    Add MockMvc testcase for demo-controller

--- src/test/java/at/fhtw/bic/slmstudyproject/SlmStudyProjectApplicationTests.java
+++ src/test/java/at/fhtw/bic/slmstudyproject/SlmStudyProjectApplicationTests.java
@@ -1,13 +1,29 @@
 package at.fhtw.bic.slmstudyproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class SlmStudyProjectApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+    
     @Test
-    void contextLoads() {
+    void getUptimeMinutes() {
+    }
+    
+    @Test
+    void getUptimeHours() throws Exception {
+        mockMvc.perform(get("/uptime/hours"))
+                .andExpect(status().is(200))
+                .andExpect(content().contentType("double"));
     }
 
 }
