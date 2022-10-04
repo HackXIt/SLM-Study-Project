4786eae225a0e8b91949b61d8110bdc2a2ad80ce
HackXIt <hackxit@gmail.com>
2022-10-04 19:43:40 +0200

    Add test for calling /api/message/reset

--- src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageResetTests.java
+++ src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageResetTests.java
@@ -5,14 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = MessageController.class)
-@SpringBootTest
 public class MessageResetTests {
     
     @Autowired
     MockMvc mockMvc;
     
-    
+    @Test
+    void MessageResetTest() throws Exception {
+        mockMvc.perform(get("/api/message/reset"))
+                .andExpect(status().is2xxSuccessful());
+    }
     
 }
