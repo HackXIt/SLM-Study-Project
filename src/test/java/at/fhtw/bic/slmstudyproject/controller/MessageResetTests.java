f3303672be518a1de065960eb3306ed8751ae8d7
HackXIt <hackxit@gmail.com>
2022-10-04 19:35:21 +0200

    Add MessageResetTests class to test /api/message/reset

---
+++ src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageResetTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmstudyproject.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = MessageController.class)
+@SpringBootTest
+public class MessageResetTests {
+    
+}
