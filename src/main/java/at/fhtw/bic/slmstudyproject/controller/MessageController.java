db164d889d952412912561f5d8cd807e5dd1ab15
HackXIt <hackxit@gmail.com>
2022-10-04 19:52:34 +0200

    Implement /api/message/reset in MessageController

--- src/main/java/at/fhtw/bic/slmstudyproject/controller/MessageController.java
+++ src/main/java/at/fhtw/bic/slmstudyproject/controller/MessageController.java
@@ -1,4 +1,24 @@
 package at.fhtw.bic.slmstudyproject.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/message")
 public class MessageController {
+    
+    private final String apiMessageDefault = "Service Ok";
+    private String currentApiMessage;
+    
+    @GetMapping("/reset")
+    public ResponseEntity<String> MessageReset() {
+        currentApiMessage = apiMessageDefault;
+        return new ResponseEntity<>(currentApiMessage, HttpStatus.OK);
+    }
+    
 }
