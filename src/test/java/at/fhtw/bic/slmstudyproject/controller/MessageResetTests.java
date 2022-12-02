6140f1d7868987ba3c816bf6910d075cfbfefcd1
HackXIt <hackxit@gmail.com>
2022-12-02 12:33:42 +0100

    Fix #20 (string comparison of content false positive)

--- src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageResetTests.java
+++ src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageResetTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = MessageController.class)
@@ -30,10 +31,10 @@ public class MessageResetTests {
     void GetMessageDefaultTest() throws Exception {
         mockMvc.perform(get("/api/message/default?msg=Hello"))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(result -> result.getResponse().getContentAsString().contentEquals("Hello"));
+                .andExpect(content().string("Hello"));
         mockMvc.perform(get("/api/message/default?msg="))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(result -> result.getResponse().getContentAsString().isBlank());
+                .andExpect(content().string(""));
     }
     
     @Test
@@ -41,11 +42,10 @@ public class MessageResetTests {
     void GetMessageResetWithoutDefaultTest() throws Exception {
         mockMvc.perform(get("/api/message/default?msg="))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(result -> result.getResponse().getContentAsString().isBlank());
+                .andExpect(content().string(""));
         mockMvc.perform(get("/api/message/reset"))
                 .andExpect(status().is5xxServerError())
-                .andExpect(result -> result.getResponse().getContentAsString().contentEquals("Default message is not set."));
-                
+                .andExpect(content().string("Default message is not set."));
     }
     
 }
--- src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageTests.java
+++ src/test/java/at/fhtw/bic/slmstudyproject/controller/MessageTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = MessageController.class)
@@ -23,18 +24,23 @@ public class MessageTests {
     void GetMessageTest() throws Exception {
         mockMvc.perform(get("/api/message"))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(result -> result.getResponse().getContentAsString().contentEquals("Status Ok"));
+                .andExpect(content().string("Status Ok"));
     }
 
+    /* NOTE: Please be aware, that this test may unexpectedly fail, when executed after the MessageResetTests
+     * This is due to a problem with the test order and the webserver retaining internal state during execution
+     * Since we didn't have the ability to change the current message at will during creation, we left this as is.
+     * When testing this case, it should be executed either exclusively or before the MessageResetTests
+     */
     @Test
     @Order(2)
     void GetMessageWithoutDefaultTest() throws Exception {
         mockMvc.perform(get("/api/message/default?msg="))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(result -> result.getResponse().getContentAsString().isBlank());
+                .andExpect(content().string(""));
         mockMvc.perform(get("/api/message"))
                 .andExpect(status().is5xxServerError())
-                .andExpect(result -> result.getResponse().getContentAsString().contentEquals("No default message or message set"));
+                .andExpect(content().string("No default message or message set"));
     }
     
 }
